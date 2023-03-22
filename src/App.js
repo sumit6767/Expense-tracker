c3abe83c27957d318d7f3f6c32ec4eed5b380dd3
sumit <sumitvishwakarma6767@gmail.com>
2023-03-22 17:21:47 +0530

    props concept uses for component reusability with different data and give different output via looping using map method

--- src/App.js
+++ src/App.js
@@ -4,10 +4,18 @@ import ReactDOM from 'react-dom/client'
 import ExpenseItem from './components/ExpenseItem'
 
 function App(props){
+
+  const expenses = [{date  : new Date().toISOString(), expenseDetail : "food" ,amount : "10" ,LocationOfExpenditure : "kalyan"},
+  {date  : new Date().toISOString(), expenseDetail : "Petrol" ,amount : "100" ,LocationOfExpenditure : "Kurla"},
+  {date  : new Date().toISOString(), expenseDetail : "Movies" ,amount : "200" ,LocationOfExpenditure : "Thane"}]
   return(
     <div>
       <h1>Expense Details</h1>
-      <ExpenseItem></ExpenseItem>
+      {
+      expenses.map(expense=>{
+        return <ExpenseItem date = {expense.date} expenseDetail = {expense.expenseDetail} amount = {expense.amount} LocationOfExpenditure = {expense.LocationOfExpenditure}></ExpenseItem>
+      })
+      }
     </div>
   )
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,13 @@
 import './expense.css'
 
 export default function ExpenseItem(props){
-    const date = new Date().toDateString()
-    const expenseDetail = "food";
-    const amount = "10"
     return(
         <div className="expense_description">
-            <div>{date}</div>
+            <div>{props.date}</div>
             <div className="expense">
-                <h2>{expenseDetail}</h2>
-                <span className="amount">{amount}</span>
+                <h2>{props.expenseDetail}</h2>
+                <span className="amount">{props.amount}</span>
+                <span className="amount">{props.LocationOfExpenditure}</span>
             </div>
         </div>
     )
