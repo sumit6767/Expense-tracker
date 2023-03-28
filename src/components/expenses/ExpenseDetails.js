ec0118f2520bf242784e11a3f880c8224604473e
sumit <sumitvishwakarma6767@gmail.com>
2023-03-28 18:16:00 +0530

    add 100 $ amount in expense feature done by useState hook of react which allows component instance to re rendering and visual change on UI on click event

--- src/components/expenses/ExpenseDetails.js
+++ src/components/expenses/ExpenseDetails.js
@@ -1,16 +1,30 @@
 import './expense.css';
+import React,{useState} from 'react';
+//named import and default import
 
 function ExpenseDetails(props){
+    let [title,setTitle] = useState(props.expenseDetail);
 
+    const[amount,setAmount] = useState(+props.amount)
     function deleteHandler(e){
         e.target.parentElement.parentElement.parentElement.remove();
     }
 
+    function updateHandler(e){
+        setTitle("updated");
+        console.log(title);
+    }
+
+    function amountHandler(){
+        setAmount(+amount+100);
+    }
     return(<div className="expense">
-    <h2>{props.expenseDetail}</h2>
-    <span className="amount">{props.amount}</span>
+    <h2>{title}</h2>
+    <span className="amount">{amount}</span>
     <span className="amount">{props.LocationOfExpenditure}</span>
     <span><button className='btn' onClick={deleteHandler}>Delete Expense</button></span>
+    <span><button className='btn' onClick={updateHandler}>Update Expense</button></span>
+    <span><button className='btn' onClick={amountHandler}>Add Expense Amount</button></span>
     </div>)
 }
 
